be79af5bed2ba9dbef8007b7ce7b5ceae72dd8c9
GZhu-tvnz <gary.zhu@tvnz.co.nz>
2019-08-29 15:56:24 +1200

    https://www.hackerrank.com/challenges/ctci-ransom-note/problem

---
+++ dictionaries-and-hashmaps/ransomNote.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// Complete the checkMagazine function below.
+func checkMagazine(magazine []string, note []string) {
+	countMap := make(map[string]int)
+	for _, word := range magazine {
+		if count, ok := countMap[word]; !ok {
+			countMap[word] = 1
+		} else {
+			countMap[word] = count + 1
+		}
+	}
+
+	for _, word := range note {
+		if count, ok := countMap[word]; !ok {
+			fmt.Println("No")
+			return
+		} else {
+			if count == 1 {
+				delete(countMap, word)
+			} else {
+				countMap[word] = count - 1
+			}
+		}
+	}
+
+	fmt.Println("Yes")
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 1024*1024)
+
+	mn := strings.Split(readLine(reader), " ")
+
+	mTemp, err := strconv.ParseInt(mn[0], 10, 64)
+	checkError(err)
+	m := int32(mTemp)
+
+	nTemp, err := strconv.ParseInt(mn[1], 10, 64)
+	checkError(err)
+	n := int32(nTemp)
+
+	magazineTemp := strings.Split(readLine(reader), " ")
+
+	var magazine []string
+
+	for i := 0; i < int(m); i++ {
+		magazineItem := magazineTemp[i]
+		magazine = append(magazine, magazineItem)
+	}
+
+	noteTemp := strings.Split(readLine(reader), " ")
+
+	var note []string
+
+	for i := 0; i < int(n); i++ {
+		noteItem := noteTemp[i]
+		note = append(note, noteItem)
+	}
+
+	checkMagazine(magazine, note)
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
